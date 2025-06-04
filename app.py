679ab250e758500aa1c6e8544129f7d85996d81f
Kumari Ayushi <ayushisomya1611@gmail.com>
2025-06-05 00:37:50 +0530

    Cleaned the data and code

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import pickle as pk
 import streamlit as st
 
 # Load the trained model
-model = pk.load(open('C:/Users/bipin/predicting house price/House_Price_Prediction.pkl', 'rb'))
+model = pk.load(open('C:/Users/bipin/predicting house price/House_Prediction_model1.pkl', 'rb'))
 
 # Title of the app
 st.header('House Price Prediction')
 
 # Load the cleaned data
-data = pd.read_csv('C:/Users/bipin/predicting house price/Cleaned_data.csv')
+data = pd.read_csv('C:/Users/bipin/predicting house price/Cleaned_data1.csv')
 
 # User input: Select location
 loc = st.selectbox('Choose the location', data['location'].unique())
@@ -30,10 +30,23 @@ balc = st.number_input('Enter no of Balconies', min_value=0)
 input_data = pd.DataFrame([[loc, sqft, bath, balc, beds]], 
                           columns=['location', 'total_sqft', 'bath', 'balcony', 'bedrooms'])
 
+def format_price(price):
+    if price >= 1e7:
+        return f"{price / 1e7:.2f} Cr"
+    elif price >= 1e5:
+        return f"{price / 1e5:.2f} Lakh"
+    else:
+        return f"{price:.2f}"
+
 # Prediction button
 if st.button("Predict Price"):
     output = model.predict(input_data)
-    
-    # Displaying the output
+    predicted_price = output[0] * 100000
+    formatted_price = format_price(predicted_price)
     st.markdown("### Prediction Result")
-    st.success(f"Price of the House is ₹{output[0] * 100000:.2f}")
+    st.success(f"Price of the House is ₹{formatted_price}")
+# Add sidebar
+st.sidebar.title("About")
+st.sidebar.text("Created by Kumari Ayushi")
+st.sidebar.text("GitHub Repository")
+st.sidebar.write("[GitHub Repo Link](https://github.com/Ayushi8757/Predicting-House-Price-)")
